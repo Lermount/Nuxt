d6eed1187a69dd711a041095ecf83af747153a23
Lermount <66447975+Lermount@users.noreply.github.com>
2024-03-03 14:12:27 +0300

    nuxt-crash-course

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <div class="container">
+        <Nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar'
+
+export default {
+
+  components: {
+    Navbar
+  }
+
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est dolores sunt totam magni repudiandae. Ut praesentium hic qui dolores commodi!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam dolorum fuga reiciendis rerum odio neque?</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </section>
+</template>
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
